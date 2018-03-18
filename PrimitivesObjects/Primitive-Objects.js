ef556c0f39e667f87fbf1f6874bcdb4bb9bdd4df
Sandip Pal <sandip.pal18@gmail.com>
2018-03-18 20:14:22 +0530

    Added PrimitiveObjects

---
+++ PrimitivesObjects/Primitive-Objects.js
@@ -0,0 +1,19 @@
+console.log("sandip".length);
+
+console.log(new String("sandip").length);
+
+var obj = new String("sandip");
+console.log(typeof obj);
+console.log(obj.valueOf());
+console.log(typeof obj.valueOf());
+
+
+var num = new Number(10);
+console.log(typeof num);
+console.log(typeof 10);
+console.log(typeof Number(10));
+console.log(typeof num.valueOf());
+
+var sum = num + 10;
+console.log(typeof sum);
+console.log(typeof num);
